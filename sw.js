afa7645201d5359fcd771b6ddeac2875703aad4c
jujofrave-art <jujofrave@gmail.com>
2026-08-14 19:34:52 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@
  * en aparecer.
  */
 
-var CACHE = 'cuentas-firebase-2026.08.14-H';
+var CACHE = 'cuentas-firebase-2026.08.14-I';
 var BASE = new URL('./', self.location).pathname;
 
 var ARCHIVOS = [
